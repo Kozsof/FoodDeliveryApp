bc8bc236768f484e68d7fa2dbd4b96e8f1e319b5
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2023-02-14 21:49:01 +0300

    Registration without checking

--- app/src/main/java/com/example/fooddeliveryapp/fragment/RegistrationFragment.java
+++ app/src/main/java/com/example/fooddeliveryapp/fragment/RegistrationFragment.java
@@ -7,16 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.fooddeliveryapp.R;
+import com.example.fooddeliveryapp.databinding.FragmentRegistrationBinding;
 
 public class RegistrationFragment extends Fragment {
-
+    FragmentRegistrationBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false);
+        binding.buttonEnter.setOnClickListener(view -> {
+            String email = binding.inputEmail.getInputText();
+            String password = binding.inputPassword.getInputText();
+            String passwordRepeat = binding.inputPasswordRepeat.getInputText();
+            String nickname = binding.inputNickname.getInputText();
+            Toast.makeText(getContext(), email + " " + password, Toast.LENGTH_SHORT).show();
+        });
+        return binding.getRoot();
     }
 }
